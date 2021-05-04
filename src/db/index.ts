602e0441362088a0bbd0fd731caf8d610878d8fa
hierror <hierrodscherer@gmail.com>
2021-05-04 20:50:46 -0300

    feat: When turned on, the server will populate the database with books if none is found

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,5 +1,23 @@
 import * as PouchDB from 'pouchdb';
+import * as books from '../data/books.json';
 
 const pouch = new PouchDB('library');
+
+pouch
+    .info()
+    .then((info) => {
+        console.log('Connected to the database');
+
+        if (Number(info.doc_count) === 0) {
+            for (const book of books) {
+                pouch.put(book);
+            }
+        }
+    })
+    .catch((err) => {
+        console.error(err);
+
+        throw new Error('Can\t connect to the database');
+    });
 
 export default pouch;
