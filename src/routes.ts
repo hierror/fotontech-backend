8c07e8d96078a5c700a655cf7fd1dc3c2ae2db5c
hierror <hierrodscherer@gmail.com>
2021-05-04 12:49:28 -0300

    feat: Created the default error handler for the API

--- src/routes.ts
+++ src/routes.ts
@@ -16,6 +16,7 @@ router.use((req, res): void => {
     res.status(404).end();
 });
 
+// Error handler for validation errors
 router.use(
     (error: Error, request: Request, res: Response, next: NextFunction) => {
         if (error instanceof ValidationError) {
@@ -32,5 +33,18 @@ router.use(
         }
     }
 );
+
+// Default error handler
+router.use((error: Error, request: Request, res: Response) => {
+    console.error(error.stack);
+
+    const payload: ResJSON = {
+        status: ResStatus.Error,
+        message: error.message || 'Internal Server Error',
+        body: [error || null]
+    };
+
+    res.status(500).json(payload);
+});
 
 export default router;
