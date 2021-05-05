02d786df3fa5cc2d1057fa83d443e6af86031b51
hierror <hierrodscherer@gmail.com>
2021-05-05 19:02:02 -0300

    config: Latest configurations related to the database, ignoring the library folder which stores all of the database data

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,5 +1,8 @@
 import * as PouchDB from 'pouchdb';
+import * as PouchDBFind from 'pouchdb-find';
 import * as books from '../data/books.json';
+
+PouchDB.plugin(PouchDBFind);
 
 const pouch = new PouchDB('library');
 
@@ -8,11 +11,17 @@ pouch
     .then((info) => {
         console.log('Connected to the database');
 
+        // Populates the database with books
         if (Number(info.doc_count) === 0) {
             for (const book of books) {
                 pouch.put(book);
             }
         }
+
+        // Create a new index to use on the queries
+        pouch.createIndex({
+            index: { fields: ['name'] }
+        });
     })
     .catch((err) => {
         console.error(err);
