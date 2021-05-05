99ba3769796e472226547f5809e305a0642d5f69
hierror <hierrodscherer@gmail.com>
2021-05-05 19:03:16 -0300

    feat: Added Swagger to generate all API Rest documentation

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,9 @@ import * as express from 'express';
 import * as cors from 'cors';
 import * as morgan from 'morgan';
 import { json, urlencoded } from 'body-parser';
+import * as SwaggerJSDoc from 'swagger-jsdoc';
+import * as SwaggerUIExpress from 'swagger-ui-express';
+import * as SwaggerConfig from './swagger/swagger.json';
 import router from './routes';
 
 const app = express();
@@ -12,6 +15,7 @@ app.use(morgan('dev'));
 app.use(cors());
 app.use(json());
 app.use(urlencoded({ extended: true }));
+app.use('/docs', SwaggerUIExpress.serve, SwaggerUIExpress.setup(SwaggerConfig));
 
 app.use('/api', router);
 
